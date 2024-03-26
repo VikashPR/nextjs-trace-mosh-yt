e713dc33e039473d61c3bb6b6ddc04ccea7e126c
VikashPR <vikashrajendran2806@gmail.com>
2024-03-26 20:58:18 +0530

    Add ProductCard component to Home page

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <>  
+      <button
+      onCLick ={() => console.log("bruh")}
+      >
+        Add to Kart
+      </button>
+    </>
+  )
+}
+
+export default ProductCard
