a11f1e7c76c37e64f164387f03785e8e5c4b1709
VikashPR <vikashrajendran2806@gmail.com>
2024-03-26 21:38:55 +0530

    Refactor fetch request in UsersPage component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
-interface User{
+interface User {
   id: number;
   name: string;
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: {
+      revalidate: 10,
+    },
+  });
   const users: User[] = await res.json();
 
   return (
     <>
-    <h1>Users</h1>
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>{user.name}</li>  ))}
-    </ul>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
