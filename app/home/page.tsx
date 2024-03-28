4b07463d5b90083373bb67ee24469bcd34b42518
VikashPR <vikashrajendran2806@gmail.com>
2024-03-28 14:52:26 +0530

    Add todo list to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import { prisma } from '@/db'
 
-const Home = () => {
+const Home = async () => {
+
+  const todos = await prisma.todo.findMany()
+
   return (
     <>
     <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
         <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
     </header>
-    
+    <ul className="pl-4">
+        {todos.map(todo => (
+            <li key={todo.id} className="flex justify-between items-center border-b border-slate-300 py-2">
+                <span>{todo.title}</span>
+                <span>{todo.completed ? '✅' : '❌'}</span>
+            </li>
+        ))}
+    </ul>
     </>
   )
 }
