168f2f214f0fb4e8c97b628f725fd9279ac58b03
VikashPR <vikashrajendran2806@gmail.com>
2024-03-28 14:42:35 +0530

    Add header with Todos and New link

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <header>
+        <h1 className="text-2xl">Todos</h1>
+        <Link href="/new">New</Link>
+    </header>
+    </>
   )
 }
 
