d31e749ce56a5430901e2785914a8f0721d27b70
VikashPR <vikashrajendran2806@gmail.com>
2024-03-28 14:46:51 +0530

    Update header styling in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,10 +4,11 @@ import Link from 'next/link'
 const Home = () => {
   return (
     <>
-    <header>
+    <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
-        <Link href="/new">New</Link>
+        <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
     </header>
+    
     </>
   )
 }
